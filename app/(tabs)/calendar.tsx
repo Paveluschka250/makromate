f8bf279187c923d075c3f7886ffe15032b368658
99Jannis99 <Jannis-rolli@msn.com>
2026-02-11 01:14:47 +0100

    chore: Update calendar and foodlist components with styles for improved UI and user experience

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const calendar = () => {
   return (
-    <View>
-      <Text>calendar</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Kalender Content WIP</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#102116",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
 
 export default calendar;
--- app/(tabs)/foodlist.tsx
+++ app/(tabs)/foodlist.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const foodlist = () => {
   return (
-    <View>
-      <Text>foodlist</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Foodlist Content WIP</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#102116",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
 export default foodlist;
